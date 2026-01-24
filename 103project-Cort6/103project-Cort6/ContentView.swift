7cab104fcb12091534190b5eca2943b2e396bd1d
Macman17 <Naqui17@gmail.com>
2026-01-24 12:47:01 -0500

    First Commit

---
+++ 103project-Cort6/103project-Cort6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103project-Cort6
+//
+//  Created by Naqui Darby on 1/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103project-Cort6/103project-Cort6/_03project_Cort6App.swift
@@ -0,0 +1,17 @@
+//
+//  _03project_Cort6App.swift
+//  103project-Cort6
+//
+//  Created by Naqui Darby on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct _03project_Cort6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
