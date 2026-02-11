be1cae21e44b1b2d5c2cc385858e006a08a3aaa2
Macman17 <Naqui17@gmail.com>
2026-02-10 19:09:14 -0600

    Assignment 4 Part 2

---
+++ 104project-Cort6/104project-Cort6/views/StepsView.swift
@@ -0,0 +1,18 @@
+//
+//  StepsView.swift
+//  104project-Cort6
+//
+//  Created by Naqui Darby on 2/5/26.
+//
+
+import SwiftUI
+
+struct StepsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepsView()
+}
