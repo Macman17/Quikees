3c4aa04c0a46a89f5ed4cae2187ed7bbbd3b289e
Macman17 <Naqui17@gmail.com>
2026-02-24 15:29:52 -0500

    Final Report 105

---
+++ 105project-cort6/BookManager/BookManager/Views/Components/FavoritetoggleView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritetoggleView.swift
+//  BookManager
+//
+//  Created by Naqui Darby on 2/21/26.
+//
+
+import SwiftUI
+
+struct FavoritetoggleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritetoggleView()
+}
---
+++ 105project-cort6/BookManager/BookManager/Views/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  BookManager
+//
+//  Created by Naqui Darby on 2/21/26.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
