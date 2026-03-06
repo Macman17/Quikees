8d00c83076b8106558027dad3df6d7fdae0a0bd7
Macman17 <Naqui17@gmail.com>
2026-03-06 15:16:43 -0500

    Assignment #3

---
+++ 105project-cort6/BookManager/BookManager/Modal/PersistentBook.swift
@@ -0,0 +1,8 @@
+//
+//  PersistentBook.swift
+//  BookManager
+//
+//  Created by Naqui Darby on 3/3/26.
+//
+
+import Foundation
---
+++ 105project-cort6/BookManager/BookManager/Views/Components/ImageField.swift
@@ -0,0 +1,18 @@
+//
+//  ImageField.swift
+//  BookManager
+//
+//  Created by Naqui Darby on 3/3/26.
+//
+
+import SwiftUI
+
+struct ImageField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageField()
+}
