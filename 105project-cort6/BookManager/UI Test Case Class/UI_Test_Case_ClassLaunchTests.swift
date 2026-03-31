e941fc2396d439d1848f2c4042d5f0553bd21273
Macman17 <Naqui17@gmail.com>
2026-03-31 08:06:24 -0400

    final report 107

---
+++ 105project-cort6/BookManager/UI Test Case Class/UI_Test_Case_ClassLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  UI_Test_Case_ClassLaunchTests.swift
+//  UI Test Case Class
+//
+//  Created by Naqui Darby on 3/31/26.
+//
+
+import XCTest
+
+final class UI_Test_Case_ClassLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
