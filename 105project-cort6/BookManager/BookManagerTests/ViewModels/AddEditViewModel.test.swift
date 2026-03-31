19ecca7a3a2a45b6d0235842777bafaa550f876b
Macman17 <Naqui17@gmail.com>
2026-03-30 22:26:30 -0400

    assignment #3 part 2

--- 105project-cort6/BookManager/BookManagerTests/ViewModels/AddEditViewModel.test.swift
+++ 105project-cort6/BookManager/BookManagerTests/ViewModels/AddEditViewModel.test.swift
@@ -20,18 +20,30 @@ struct AddEditViewModel_test {
         )
         
         let modelContext = container.container.mainContext
+        let Book = PersistentBook(
+            title: "Test Title",
+            author: "Author",
+            genre: .unknown,
+            summary: "Summary",
+            isFavorite: false,
+            rating: 0,
+            readingStatus: .unknown,
+            review: "Review",
+            coverData: nil
+            
+        )
         
         let viewModel = AddEditViewModel(
             modelContext: modelContext)
         
-        #expect(viewModel.title == "")
-        #expect(viewModel.author == "")
-        #expect(viewModel.genre == "")
-        #expect(viewModel.summary == "")
+        #expect(viewModel.title == "Test Title")
+        #expect(viewModel.author == "Author")
+        #expect(viewModel.genre == .unknown)
+        #expect(viewModel.summary == "Summary")
         #expect(viewModel.isFavorite == false)
         #expect(viewModel.rating == 0)
-        #expect(viewModel.readingStatus == "")
-        #expect(viewModel.review == "")
+        #expect(viewModel.readingStatus == .unknown)
+        #expect(viewModel.review == "Review")
         #expect(viewModel.coverData == nil)
         #expect(viewModel.saveBook.isEnabled)
         
@@ -40,21 +52,86 @@ struct AddEditViewModel_test {
     
     @Test("Test init when editing an exisiting book")
     func testInitWithExistingBook() {
+        let container = try ModelContext(
+            for: PersistentBook.self,
+            configuration: .init(isStoredInMemoryOnly: true)
+        )
+        
+        let modelContext = container.container.mainContext
+        let Book = PersistentBook(
+            title: "Test Title",
+            author: "Author",
+            genre: .unknown,
+            summary: "Summary",
+            isFavorite: false,
+            rating: 0,
+            readingStatus: .unknown,
+            review: "Review",
+            coverData: nil
+            
+        )
+        
+        let viewModel = AddEditViewModel(
+            modelContext: modelContext)
+        
+        #expect(viewModel.title == "Test Title")
+        #expect(viewModel.author == "Author")
+        #expect(viewModel.genre == .unknown)
+        #expect(viewModel.summary == "Summary")
+        #expect(viewModel.isFavorite == false)
+        #expect(viewModel.rating == 0)
+        #expect(viewModel.readingStatus == .unknown)
+        #expect(viewModel.review == "Review")
+        #expect(viewModel.coverData == nil)
+        #expect(viewModel.saveBook.isEnabled)
+        
+        
         
     }
     
     @Test("Test that navigation title is 'Add Book' when creating")
     func testNavigationForNewBook() {
-        
+        let viewModel = AddEditViewModel()
+        #expect(viewModel.navigationTitle == "Add Book")
     }
     
     @Test("Test that navigation title is 'Edit Book' when editing")
     func testNavigationForNewBook() {
-        
+        let viewModel = AddEditViewModel()
+        #expect(viewModel.navigationTitle == "Edit Book")
     }
     
     @Test("Test isNotSavable works for a new book")
     func testNavigationForNewBook() {
+        let viewModel = AddEditViewModel()
+        #expect(viewModel.isNotSavable)
+        #expect(viewModel.saveBook.isEnabled == false)
+    }
+    
+    @Test("Test isNotSavable works for a existing book")
+    func testNavigationForExistingBook() {
+        let viewModel = AddEditViewModel()
+        #expect(viewModel.isNotSavable)
+        #expect(viewModel.saveBook.isEnabled == false)
+        #expect(viewModel.navigationTitle == "Edit Book")
         
     }
+    
+    @Test("SaveBook adds a new book")
+    func testSaveBookAddsNewBook() {
+        let viewModel = AddEditViewModel()
+        #expect(viewModel.saveBook().isEmpty)
+        #expect(viewModel.navigationTitle == "Add Book")
+        
+    }
+    
+    @Test("SaveBook updates an existing book")
+    func testSaveBookUpdatesExistingBook() {
+        let viewModel = AddEditViewModel()
+        
+        #expect(viewModel.saveBook().isEmpty)
+        #expect(viewModel.navigationTitle == "Edit Book")
+        
+    }
+    
 }
