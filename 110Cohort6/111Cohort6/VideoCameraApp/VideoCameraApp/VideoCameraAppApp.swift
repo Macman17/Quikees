fe9f78fd60fc54dff1f3f64a2bfaa30fcaeb17e0
Macman17 <Naqui17@gmail.com>
2026-04-26 23:56:24 -0400

    111 assignment #2

---
+++ 110Cohort6/111Cohort6/VideoCameraApp/VideoCameraApp/VideoCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCameraAppApp.swift
+//  VideoCameraApp
+//
+//  Created by Naqui Darby on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
